4b50edab67caed1ef1018e310afd08486fb8d5dd
mpbmke <33264670+mpbmke@users.noreply.github.com>
2022-04-04 19:31:24 -0500

    Start/Quit functionality
    Main menu created with Start/Quit buttons hooked up

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
